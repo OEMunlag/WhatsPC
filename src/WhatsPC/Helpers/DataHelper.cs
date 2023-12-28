d562d9a153e16b4211b3e24cad9256ea3b6125ed
Belim <belim@builtbybel.com>
2023-12-28 18:46:40 +0100

    Add helpers

---
+++ src/WhatsPC/Helpers/DataHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WhatsPC;
+
+namespace DataHelper
+{
+    internal class Data
+    {
+        public static string DataRootDir = Application.StartupPath +
+                                         @"\app\";
+    }
+
+    public static class Uri
+    {
+        public const string URL_BUILTBYBEL = "https://www.builtbybel.com";
+        public const string URL_ASSEMBLY = "https://raw.githubusercontent.com/builtbybel/WhatsPC/main/src/WhatsPC/Properties/AssemblyInfo.cs";
+        public const string URL_GITREPO = "https://github.com/builtbybel/WhatsPC";
+        public const string URL_ICONATTRIBUTION = "https://www.flaticon.com/free-icon/toaster_7175212";
+        public const string URL_GITLATEST = URL_GITREPO + "/releases/latest";
+    }
+}
---
+++ src/WhatsPC/Helpers/Logger.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace WhatsPC
+{
+    public class Logger
+    {
+        private WebView2 webView;
+
+        public Logger(WebView2 webView)
+        {
+            this.webView = webView;
+        }
+
+  
+
+        // Log method for a list of strings
+        public void Log(List<string> messages, Color color)
+        {
+            if (webView.InvokeRequired)
+            {
+                webView.Invoke(new Action(() => Log(messages, color)));
+                return;
+            }
+
+            foreach (var message in messages)
+            {
+                LogToWebView(message, color);
+            }
+        }
+
+        // Log method for a single string
+        public void Log(string message, Color color)
+        {
+            if (webView.InvokeRequired)
+            {
+                webView.Invoke(new Action(() => Log(message, color)));
+                return;
+            }
+
+            LogToWebView(message, color);
+        }
+
+        // Helper method to add log entry to WebView2
+        private void LogToWebView(string message, Color color)
+        {
+            string formattedMessage = $"<div style='color:{ColorToHex(color)}'>{message}</div>";
+            webView.CoreWebView2.PostWebMessageAsString(formattedMessage);
+
+            // Some JS to append formatted message to logContainer
+            webView.ExecuteScriptAsync($"document.getElementById('logContainer').innerHTML += '{formattedMessage}';");
+
+            // Scroll down after adding new content
+            webView.ExecuteScriptAsync("window.scrollTo(0, document.body.scrollHeight);");
+        }
+
+        // Helper method to convert Color to hexadecimal representation
+        private string ColorToHex(Color color)
+        {
+            return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+    }
+}
